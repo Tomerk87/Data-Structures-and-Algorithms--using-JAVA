68cc3dfc210ffbb2720bb513de44688fb3f7f55a
Tomerk87 <Kfir.tomer@gmail.com>
2020-08-23 11:10:07 +0300

    Changes to Graph class
    From HashTable to HashMap. HashTable is thread safe, but slower. We only have a single thread so no need for it, and HashMap has a better implementation when handling collision
    addEdge added a check to make sure we only set an edge for vertex we actually added to our Graph.
    showConnection better for looping on the map, no need to create an additional array and unnecessary conversion.

--- src/data_structures/graphs/Graph.java
+++ src/data_structures/graphs/Graph.java
@@ -6,7 +6,7 @@ import java.util.Hashtable;
 
 public class Graph {
     int numberOfNodes = 0;
-    Hashtable<Integer, ArrayList<Integer>> adjacentList = new Hashtable<>();
+    Map<Integer, ArrayList<Integer>> adjacentList = new HashMap<>();
 
     public void addVertex(int node) {
         adjacentList.put(node, new ArrayList<>());
@@ -14,14 +14,15 @@ public class Graph {
     }
 
     public void addEdge(int node1, int node2) {
-        adjacentList.get(node1).add(node2);
-        adjacentList.get(node2).add(node1);
+        if(adjacentList.containsKey(node1) && adjacentList.containsKey(node2) {
+            adjacentList.get(node1).add(node2);
+            adjacentList.get(node2).add(node1);
+        }
     }
 
     public void showConnections() {
-        Object[] keys = adjacentList.keySet().toArray();
-        for (Object key : keys) {
-            System.out.println(key + " ---> " + adjacentList.get(Integer.parseInt(key.toString())));
+        for(Map.Entry entry : adjacentList.entrySet() {
+            System.out.println(entry.getKey() + " ---> " + entry.getValue());
         }
     }
 
